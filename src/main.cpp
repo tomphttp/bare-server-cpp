260a810ea9980171938bf29419ea94c6eef1ce7f
David Reed <x@sys32.dev>
2022-02-20 18:39:38 -0500

    adjust

--- src/main.cpp
+++ src/main.cpp
@@ -20,114 +20,54 @@ using tcp = boost::asio::ip::tcp;
 // 1024 squared
 constexpr unsigned long long MEGABYTE = 1048576;
 
-class http_connection : public std::enable_shared_from_this<http_connection> {
+class Serving : public std::enable_shared_from_this<Serving> {
 public:
-	http_connection(tcp::socket socket) : socket_(std::move(socket)) {}
-	// Initiate the asynchronous operations associated with the connection.
-	void start() {
-		read_request();
-		check_deadline();
+	Serving(tcp::socket s) : socket(std::move(s)) {}
+	void process(){
+		read();
 	}
 private:
-	// The socket for the currently connected client.
-	tcp::socket socket_;
-
-	// The buffer for performing reads.
-	beast::flat_buffer buffer_{8192};
-
-	// The request message.
-	http::request<http::dynamic_body> request_;
-
-	// The response message.
-	http::response<http::dynamic_body> response_;
-
+	tcp::socket socket;
+	beast::flat_buffer buffer{8192};
+	http::request<http::dynamic_body> request;
+	http::response<http::dynamic_body> response;
+	// net::steady_timer deadline{ socket.get_executor(), std::chrono::seconds(60) };
 	// The timer for putting a deadline on connection processing.
-	net::steady_timer deadline_{ socket_.get_executor(), std::chrono::seconds(60) };
-
-	// Asynchronously receive a complete request message.
-	void read_request() {
-		auto self = shared_from_this();
-
-		http::async_read(socket_, buffer_, request_, [self](beast::error_code ec, std::size_t bytes_transferred) {
+	/*deadline.async_wait([&socket](beast::error_code ec) {
+		if(!ec) {
+			// Close socket to cancel any outstanding operation.
+			// socket.close(ec);
+		}
+	});*/
+	void read(){ // headers and body
+		std::shared_ptr<Serving> serving = shared_from_this();
+	
+		http::async_read(socket, buffer, request, [serving](beast::error_code ec, std::size_t bytes_transferred) {
 			boost::ignore_unused(bytes_transferred);
+			
 			if (!ec) {
-				self->process_request();
+				serving->respond();
+			}else{
+				std::cerr << "error code " << ec.category().message(1) << ", " << ec.value() << std::endl;
 			}
 		});
 	}
+	void respond(){
+		std::cout << request.target().to_string() << std::endl;
 
-	// Determine what needs to be done with the request message.
-	void process_request() {
-		response_.version(request_.version());
-		response_.keep_alive(false);
+		response.result(http::status::not_found);
+		response.set(http::field::content_type, "text/plain");
+		beast::ostream(response.body()) << "File not found";
+		response.content_length(response.body().size());
 
-		const std::string target = request_.target().to_string();
-		
-		if(target == "/v1/"){
-			
-		}
-		else if(target == "/v1/ws-meta"){
-
-		}
-		else if(target == "/"){
-			rapidjson::Document document;
-			document.SetObject();
-			
-			rapidjson::Document::AllocatorType& allocator = document.GetAllocator();
-
-			rapidjson::Value versions;
-			versions.SetArray();
-			versions.PushBack(rapidjson::Value().SetString("v1", allocator), allocator);
-			document.AddMember(rapidjson::Value().SetString("versions", allocator), versions, allocator);
-
-			document.AddMember(rapidjson::Value().SetString("language", allocator), rapidjson::Value().SetString("C++", allocator), allocator);
-
-			double vm = 0.0;
-			process_memory_usage(vm);
-			
-			document.AddMember(rapidjson::Value().SetString("memoryUsage", allocator), rapidjson::Value().SetDouble(vm / MEGABYTE), allocator);
-
-			rapidjson::StringBuffer buffer;
-			rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-
-			document.Accept(writer);
-
-			std::string serialized = buffer.GetString();
-
-			response_.set("content-type", "application/json");
-			beast::ostream(response_.body()) << serialized;
-
-			write_response();
-
-			return;
-		}
-		
-		response_.result(http::status::not_found);
-		response_.set(http::field::content_type, "text/plain");
-		beast::ostream(response_.body()) << "File not found";
-		write_response();
+		write();
 	}
-	// Asynchronously transmit the response message.
-	void write_response() {
-		auto self = shared_from_this();
+	void write(){
+		std::shared_ptr<Serving> serving = shared_from_this();
 
-		response_.content_length(response_.body().size());
-
-		http::async_write(socket_, response_, [self](beast::error_code ec, std::size_t) {
-			self->socket_.shutdown(tcp::socket::shutdown_send, ec);
-			self->deadline_.cancel();
-		});
-	}
-
-	// Check whether we have spent enough time on this connection.
-	void check_deadline() {
-		auto self = shared_from_this();
-
-		deadline_.async_wait([self](beast::error_code ec) {
-			if(!ec) {
-				// Close socket to cancel any outstanding operation.
-				self->socket_.close(ec);
-			}
+		http::async_write(socket, response, [serving](beast::error_code ec, std::size_t) {
+			serving->socket.shutdown(tcp::socket::shutdown_send, ec);
+			// deadline.cancel();
 		});
 	}
 };
@@ -135,9 +75,7 @@ private:
 // "Loop" forever accepting new connections.
 void http_server(tcp::acceptor& acceptor, tcp::socket& socket) {
 	acceptor.async_accept(socket, [&](beast::error_code ec) {
-		if(!ec) {
-			std::make_shared<http_connection>(std::move(socket))->start();
-		}
+		std::make_shared<Serving>(std::move(socket))->process();
 
 		http_server(acceptor, socket);
 	});
