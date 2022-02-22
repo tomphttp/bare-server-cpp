161b667ef84ced8e6950ef2d17d3188b795ed970
David Reed <x@sys32.dev>
2022-02-21 20:39:30 -0500

    wip

--- src/Serving.h
+++ src/Serving.h
@@ -7,12 +7,12 @@ public:
 	boost::asio::ip::tcp::socket socket;
 	boost::beast::http::request<boost::beast::http::dynamic_body> request;
 	boost::beast::http::response<boost::beast::http::dynamic_body> response;
+	boost::beast::flat_buffer buffer;
+	boost::asio::steady_timer deadline;
 	Serving(boost::asio::ip::tcp::socket socket, std::shared_ptr<Server> server);
 	void process();
 	void write();
 private:
-	boost::beast::flat_buffer buffer;
-	boost::asio::steady_timer deadline;
 	void init_deadline();
 	void read();
 	void respond();
--- src/api/v1/v1_http_proxy.cpp
+++ src/api/v1/v1_http_proxy.cpp
@@ -7,20 +7,32 @@ namespace net = boost::asio;
 namespace ssl = boost::asio::ssl;
 using tcp = net::ip::tcp;
 
+struct SessionBody {
+	using value_type = std::string;
+	struct SessionReader {
+		template<bool isRequest, class Headers>
+		SessionReader(http::message<isRequest, SessionBody, Headers>& m){}
+		void write(void const* data, std::size_t size, beast::error_code& ec) {
+			std::cout << "Write  " << size << std::endl;
+			// this will be called with each piece of the body, after chunk decoding
+		}
+	};
+};
+
+
 class Session : public std::enable_shared_from_this<Session> {
 public:
 	Session(std::shared_ptr<Serving> _serving)
 		: serving(_serving)
 		, resolver(net::make_strand(serving->server->iop))
 		, stream(serving->server->iop)
+		, serializer(response)
 	{}
 	std::shared_ptr<Serving> serving;
 	tcp::resolver resolver;
 	beast::tcp_stream stream;
 	beast::flat_buffer buffer; // (Must persist between reads)
 	char read_buffer[8000];
-	http::request<http::empty_body> request;
-	http::response<http::string_body> response;
 	void process(std::string host, std::string port, std::string path, int version){
 		request.version(version);
 		request.method(http::verb::get);
@@ -31,6 +43,11 @@ public:
 		resolver.async_resolve(host, port, beast::bind_front_handler(&Session::on_resolve, shared_from_this()));
 	}
 private:
+	SessionBody body;
+	http::request<http::empty_body> request;
+	http::response<http::buffer_body> response;
+	http::response_serializer<http::buffer_body, http::fields> serializer;
+	http::response_parser<http::buffer_body> remote_parser;
 	void on_resolve(beast::error_code ec, tcp::resolver::results_type results) {
 		if (ec) {
 			return fail(ec, "resolve");
@@ -51,9 +68,8 @@ private:
 		stream.expires_after(std::chrono::seconds(30));
 
 		// Send the HTTP request to the remote host
-		http::async_write(stream, request,beast::bind_front_handler(&Session::on_write, shared_from_this()));
+		http::async_write(stream, request, beast::bind_front_handler(&Session::on_write, shared_from_this()));
 	}
-	http::response_parser<http::string_body> remote_parser;
 	void on_write(beast::error_code ec, size_t bytes_transferred) {
 		boost::ignore_unused(bytes_transferred);
 
@@ -64,8 +80,6 @@ private:
 		
 		http::async_read_header(stream, buffer, remote_parser, beast::bind_front_handler(&Session::on_headers, shared_from_this()));
 	}
-	http::message<false, http::empty_body, http::fields> message{http::status::ok, 11};
-	http::response_serializer<http::empty_body> serializer{message};
 	void on_headers(beast::error_code ec, size_t bytes_transferred){
 		if(ec) {
 			return fail(ec, "read");
@@ -78,51 +92,71 @@ private:
 		}
 
 		std::cout << "got keepalive was " << got.keep_alive() << std::endl;
-		message.keep_alive(got.keep_alive());
-		if(got.keep_alive()){
-			message.set("Connection", "Keep-Alive");
-		}
-		message.set("X-Bare-Headers", "{}");
 		
-		message.chunked(got.chunked());
-		// message.set("transfer-encoding", "chunked");
+		response.result(http::status::ok);
+		response.version(11);
+		response.keep_alive(got.keep_alive());
 
+		if(got.keep_alive()){
+			response.set("Connection", "Keep-Alive");
+		}
+		
+		response.set("X-Bare-Headers", "{}");
+		
+    	response.set(http::field::transfer_encoding, "chunked");
+		
+		response.body().data = nullptr;
+		response.body().more = true;
+		
 		http::async_write_header(serving->socket, serializer, beast::bind_front_handler(&Session::on_client_write_headers, shared_from_this()));
+		stream.async_read_some(boost::asio::buffer(read_buffer, sizeof(read_buffer)), beast::bind_front_handler(&Session::on_read_chunk, shared_from_this()));
+
 	}
 	void on_client_write_headers(beast::error_code ec, size_t bytes_transferred){
-		stream.async_read_some(boost::asio::buffer(read_buffer, sizeof(bytes_transferred)), beast::bind_front_handler(&Session::on_read_chunk, shared_from_this()));
+		std::cout << "wrote headers, reading some" << std::endl;
 	}
+	bool shutdown = false;
 	void on_client_write_chunk(beast::error_code ec, size_t bytes_transferred){
-		std::cout << "wrote chunk " << std::endl;
-	}
-	void on_read_chunk(beast::error_code ec, size_t bytes_transferred){
-		if(ec) {
-			return fail(ec, "read");
-		}
-		
-		auto got = remote_parser.get();
-
-		if (bytes_transferred == 0) {
-			std::cout << "no bytes read..." << std::endl;
-		}else{
-			std::cout << "read " << bytes_transferred << " bytes" << std::endl;
-
-			serving->socket.async_write_some(boost::asio::buffer(read_buffer, bytes_transferred), beast::bind_front_handler(&Session::on_client_write_chunk, shared_from_this()));
-
+		if (ec == http::error::need_buffer) {
+			std::cout << "read more" << std::endl;
 			stream.async_read_some(boost::asio::buffer(read_buffer, sizeof(read_buffer)), beast::bind_front_handler(&Session::on_read_chunk, shared_from_this()));
 		}
-		/*
-		beast::ostream(serving->response.body()) << response.body();
+		else{
+			if (ec) {
+				return fail(ec, "writing chunk");
+			}
+		
+			std::cout << "wrote chunk" << std::endl;
 
-		// Gracefully close the socket
-		stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+			if(shutdown){
+				serving->socket.shutdown(tcp::socket::shutdown_send, ec);
+				serving->deadline.cancel();
+			}
+		}
+	}
+	void on_read_chunk(beast::error_code ec, size_t bytes_transferred){
+		if (ec == boost::asio::error::eof) {
+			std::cout << "eof, bytes transferred was " << bytes_transferred << std::endl;
 
-		serving->write();	
+			response.body().data = read_buffer;
+            response.body().size = bytes_transferred;
+			response.body().more = false;
 
-		// not_connected happens sometimes so don't bother reporting it.
-		if(ec && ec != beast::errc::not_connected) {
-			return fail(ec, "shutdown");
-		}*/
+			shutdown = true;
+		}
+		else {
+			if (ec) {
+				return fail(ec, "read");
+			}
+
+			std::cout << "will transfer " << bytes_transferred << " bytes" << std::endl;
+			
+			response.body().data = read_buffer;
+            response.body().size = bytes_transferred;
+            response.body().more = true;
+		}
+		
+		http::async_write(serving->socket, serializer, beast::bind_front_handler(&Session::on_client_write_chunk, shared_from_this()));
 	}
 	void fail(beast::error_code ec, std::string message){
 		std::cout << "Failed with message: " << message << ", " << ec.message() << ", " << ec.category().name() << ", " << ec.category().message(0) << std::endl;
